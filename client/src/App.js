09268889e29fccc5d3e2e03d62053590f5afbf92
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-25 10:47:24 +0000

    Finished first header draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import logo from './img/logo-black.svg'
+import logo from './img/logoBlack.png';
 import styled from 'styled-components';
 
 const Header = styled.div`
+  height: 100px;
+  padding: 25px 0;
   .logo {
     width: 100%;
     max-height: 50px;
+    img {
+      max-height: 100%;
+    }
+  }
+  .menu {
+    justify-content: flex-end;
+    a {
+      font-size: 1.5rem;
+      padding: .81rem 1.56rem;
+      //font-variant: small-caps;
+      text-transform: uppercase;
+      font-family: ${props => props.theme.fonts.secondary};
+      &:last-child {
+        margin-right: -1.56rem;
+      }
+    }
   }
 `;
 
@@ -14,13 +32,13 @@ function App() {
     <div className="App">
       <Header>
         <div className="inner flex">
-          <a className="col col-2 logo">
+          <a href="/" className="col col-2 logo">
             <img src={logo} alt="Melquip's logo" />
           </a>
           <div className="col col-2 menu flex">
-            <a href="#">About</a>
-            <a href="#">Portfolio</a>
-            <a href="#">FAQ</a>
+            <a href="/">About</a>
+            <a href="/work">Portfolio</a>
+            <a href="/faq">FAQ</a>
           </div>
         </div>
       </Header>
