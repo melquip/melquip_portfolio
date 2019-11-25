1284afceb111c2189c3f57461afbcc54e5627a1b
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-25 16:36:30 +0000

    Refactored slides to be dynamic | Added animations

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,58 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Slick from 'react-slick';
 import styled from 'styled-components';
+import Tilt from 'react-tilt';
 
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 1000,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: 5000,
-}
+const StyledAbout = styled.section`
+  overflow: hidden;
+`;
 
 const StyledSlick = styled(Slick)`
+  cursor: move;
   margin-left: -.3rem;
 `;
+
 const Slide = styled.div`
-  padding: 20px 0;
+  padding-bottom: 1rem;
   p {
-    font-size: 8.5rem;
-    line-height: 8.5rem;
-    font-weight: 300;
-    font-family: ${props => props.theme.fonts.secondary}
+    font-size: 7.5rem;
+    line-height: 8.25rem;
+    font-family: ${props => props.theme.fonts.secondary};
+    transition: transform .33s ease-in-out;
+    &:hover {
+      transform: translateX(0) scale(.98);
+    }
   }
 `;
 
+const settingsSlider = {
+  dots: true,
+  infinite: true,
+  accessibility: true,
+  speed: 1000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 4000,
+  onInit: () => {
+  }
+}
+
+const settingsTilt = {
+  reverse: false,     // reverse the tilt direction
+  max: 8,             // max tilt rotation (degrees)
+  perspective: 1000,  // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1,      // 2 = 200%, 1.5 = 150%, etc..
+  speed: 300,    // Speed of the enter/exit transition
+  transition: true,   // Set a transition on enter/exit.
+  axis: 'y',   // What axis should be disabled. Can be X or Y.
+  reset: false,    // If the tilt effect has to be reset on exit.
+  easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+}
+
 const About = (props) => {
+  const [aboutSlides, setAboutSlides] = useState([]);
+
+  useEffect(() => {
+    setAboutSlides([
+      [
+        "Hi, I'm Melqui,",
+        "22 years old,",
+        "portuguese,",
+        "full-stack developer",
+        "& gamer",
+      ],
+      [
+        "Hi, I'm Melqui,",
+        "22 years old,",
+        "portuguese,",
+        "full-stack developer",
+        "& gamer",
+      ],
+      [
+        "Hi, I'm Melqui,",
+        "22 years old,",
+        "portuguese,",
+        "full-stack developer",
+        "& gamer",
+      ]
+    ]);
+  }, []);
+
   return (
-    <section className="about">
-      <div className="inner">
-        <StyledSlick {...settings}>
-          <Slide className="slide">
-            <p>Hi, I'm a </p>
-            <p>22 years old</p>
-            <p>portuguese</p>
-            <p>full-stack developer</p>
-            <p>& gamer</p>
+    <StyledAbout className="about">
+      <StyledSlick {...settingsSlider}>
+        {aboutSlides.length ? aboutSlides.map((slide, i) => (
+          <Slide key={i}>
+            <Tilt options={settingsTilt}>
+              <div className="inner">
+                {slide.map((line, j) => <p key={j}>{line}</p>)}
+              </div>
+            </Tilt>
           </Slide>
-          <Slide className="slide">
-            <p>Hi, I'm a </p>
-            <p>22 years old</p>
-            <p>portuguese</p>
-            <p>full-stack developer</p>
-            <p>& gamer</p>
-          </Slide>
-          <Slide className="slide">
-            <p>Hi, I'm a </p>
-            <p>22 years old</p>
-            <p>portuguese</p>
-            <p>full-stack developer</p>
-            <p>& gamer</p>
-          </Slide>
-        </StyledSlick>
-      </div>
-    </section>
+        )) : null}
+      </StyledSlick>
+    </StyledAbout>
   )
 }
 
