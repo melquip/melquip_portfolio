9a4e7235d2496e5e3e473f481f7b72d265b03600
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-25 10:03:29 +0000

    Added header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import logo from './img/logo-black.svg'
+import styled from 'styled-components';
+
+const Header = styled.div`
+  .logo {
+    width: 100%;
+    max-height: 50px;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-     
+      <Header>
+        <div className="inner flex">
+          <a className="col col-2 logo">
+            <img src={logo} alt="Melquip's logo" />
+          </a>
+          <div className="col col-2 menu flex">
+            <a href="#">About</a>
+            <a href="#">Portfolio</a>
+            <a href="#">FAQ</a>
+          </div>
+        </div>
+      </Header>
     </div>
   );
 }
