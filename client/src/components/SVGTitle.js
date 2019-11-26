bfa08b2443abee56c09df74b3643ab0594006ca4
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-26 11:33:17 +0000

    Matched title animation with new logo animation

--- client/src/components/SVGTitle.js
+++ client/src/components/SVGTitle.js
@@ -3,25 +3,33 @@ import React from 'react'
 export const SVGTitle = (props) => {
   return (
     <h1>
-      <svg viewBox="0 0 1028 88">
+      <svg viewBox="0 0 1024 88">
         <defs>
-          <linearGradient id="gr-simple" x1="0" y1="0" x2="1" y2="1">
+          <linearGradient id="gr-simple" x1="0" y1="0" x2="1" y2="1" gradientTransform="rotate(-45)" fx="0">
             <stop stopColor="rgba(38, 84, 124, 1)" offset="0">
-              <animate 
-                attributeName="stop-color" 
-                values="rgba(38, 84, 124, 1); rgba(239, 71, 111, 1); rgba(239, 71, 111, 1); rgba(38, 84, 124, 1)"
-                keyTimes="0 ; 0.4 ; 0.8 ; 1"
-                dur="16s" 
-                repeatCount="indefinite" 
+              <animate
+                attributeName="stop-color"
+                values="rgba(239, 71, 111, 1); rgba(38, 84, 124, 1); rgba(239, 71, 111, 1)"
+                keyTimes="0; 0.5; 1"
+                dur="8s"
+                repeatCount="4"
               />
-              <animate 
-                attributeName="offset" 
-                values="0; 1; 1; 0" 
-                dur="16s" 
-                repeatCount="indefinite" 
+              <animate
+                attributeName="offset"
+                values="0; 1; 0"
+                dur="8s"
+                repeatCount="4"
               />
             </stop>
-            <stop stopColor="rgba(38, 84, 124, 1)" offset="1"></stop>
+            <stop stopColor="rgba(38, 84, 124, 1)" offset="1">
+              <animate
+                attributeName="stop-color"
+                values="rgba(38, 84, 124, 1); rgba(239, 71, 111, 1); rgba(38, 84, 124, 1)"
+                keyTimes="0; 0.5; 1"
+                dur="16s"
+                repeatCount="2"
+              />
+            </stop>
           </linearGradient>
         </defs>
         <text textAnchor="left"
