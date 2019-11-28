62795836e5b8339212d72716e5f60d151f8e8b9f
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-28 16:15:35 +0000

    Deployment fixes database connection

--- config/index.js
+++ config/index.js
@@ -3,6 +3,8 @@ module.exports = {
   port: process.env.PORT,
   origin: [
     'http://localhost:3000',
+    'http://www.melquip.com',
     'https://www.melquip.com'
-  ]
+  ],
+  productionDB: process.env.DATABASE_URL,
 }
