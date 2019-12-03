3b6abeabd5d9da9d40b72462294958c34584c13b
Melquisedeque Pereira <melqui1@live.com.pt>
2019-12-03 21:20:42 +0000

    Added all social media links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -35,12 +35,12 @@ const Footer = (props) => {
     <StyledFooter>
       <div className="inner flex">
         <div className="col col-2">
-          Developed by <span>melqui</span>
+          <a href="mailto:hire@melquip.com" target="_blank"><span>hire@melquip.com</span></a>
         </div>
         <div className="col col-2">
-          <a href="#" target="_blank"><i className="icon-twitter"></i></a>
-          <a href="#" target="_blank"><i className="icon-github"></i></a>
-          <a href="#" target="_blank"><i className="icon-linkedin"></i></a>
+          <a href="https://twitter.com/melquip7" target="_blank"><i className="icon-twitter"></i></a>
+          <a href="https://github.com/melquip" target="_blank"><i className="icon-github"></i></a>
+          <a href="https://www.linkedin.com/in/melquip/" target="_blank"><i className="icon-linkedin"></i></a>
         </div>
       </div>
     </StyledFooter>
