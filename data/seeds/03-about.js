c21de8f178db375ee2f4b67c7d7fa7bcb516d331
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-10 21:45:25 +0000

    Updated about seed

--- data/seeds/03-about.js
+++ data/seeds/03-about.js
@@ -3,8 +3,16 @@ exports.seed = function(knex) {
     .then(function () {
       return knex('about').insert([
         {
-          line: "Hi, I'm Melqui,|22 years old,|portuguese,|full-stack developer|& gamer",
+          line: "Hi, I'm Melqui,|23 years old,|portuguese,|full-stack|web developer",
           priority: 10,
+        },
+        {
+          line: "I've worked|in web development|for 2 years|before starting|Lambda School",
+          priority: 9,
+        },
+        {
+          line: "Now I'm looking|for remote|and exciting|new projects|to work on!",
+          priority: 8,
         }
       ]);
     });
