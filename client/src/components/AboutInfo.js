807aa448ff416a8e02eff363da37973ce0cf9799
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-13 22:59:14 +0000

    Added scroll animation

--- client/src/components/AboutInfo.js
+++ client/src/components/AboutInfo.js
@@ -19,7 +19,13 @@ const AboutInfo = (props) => {
 
   return (
     <StyledAboutInfo id="aboutInfo">
-      
+      <div className="inner">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum nesciunt animi consequuntur repellat aliquid totam! Tempore magni dolorem, blanditiis ea ad nihil hic cumque quasi fugit, expedita maxime perspiciatis sunt!
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime illo praesentium asperiores nemo delectus nobis quibusdam error eaque cumque iste? Omnis dolorem dolore aliquid nobis beatae ducimus laborum totam ab?
+        Ratione quis nisi provident excepturi molestias nostrum molestiae rem culpa, animi, recusandae quo, commodi qui consequuntur necessitatibus vero. Debitis alias natus consectetur voluptates vero illum quae sequi doloribus hic id!
+        Fugit repellat odit nostrum omnis, quia unde iusto beatae dolores aliquam repellendus provident. Aspernatur nemo earum, molestiae, nihil veniam quia dolores vel pariatur quis alias ipsam ipsa cumque sequi libero.
+        Eos doloribus mollitia veniam, fuga necessitatibus laborum sapiente, dignissimos officiis iste modi voluptate vel, molestiae corrupti consectetur aperiam rerum tempora architecto reiciendis ratione placeat sint sunt dolorum possimus. Blanditiis, ab.
+      </div>
     </StyledAboutInfo>
   )
 }
