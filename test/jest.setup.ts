19e4aeb6c2f7b51d24917d96e63af8dc6ce5292c
Melquisedeque Pereira <melqui1@live.com.pt>
2021-01-26 12:01:07 +0000

    Adjusted indentation

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -8,28 +8,28 @@ import "@testing-library/jest-dom";
 dotenv.config({ path: ".env.test" });
 
 nock("http://localhost:3000")
-    .get("/api/planetary/apod")
-    .query({ api_key: "NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo", hd: true })
-    .reply(200, {
-        copyright: "Pankod",
-        date: "2019-05-23",
-        explanation: "",
-        hdurl: "",
-        media_type: "",
-        service_version: "",
-        title: "",
-        url: "",
-    });
+  .get("/api/planetary/apod")
+  .query({ api_key: "NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo", hd: true })
+  .reply(200, {
+    copyright: "Pankod",
+    date: "2019-05-23",
+    explanation: "",
+    hdurl: "",
+    media_type: "",
+    service_version: "",
+    title: "",
+    url: "",
+  });
 
 nock("http://localhost:3000")
-    .get("/api/planetary/apod")
-    .query({ api_key: "NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo", hd: false })
-    .reply(500);
+  .get("/api/planetary/apod")
+  .query({ api_key: "NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo", hd: false })
+  .reply(500);
 
 nock("http://localhost:3000")
-    .get("/api/200")
-    .reply(200, { success: true });
+  .get("/api/200")
+  .reply(200, { success: true });
 
 nock("http://localhost:3000")
-    .get("/api/404")
-    .reply(404, { success: false });
+  .get("/api/404")
+  .reply(404, { success: false });
