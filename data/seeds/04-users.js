d46943ba7b081a27daae27f00fdabfa2d6029c2d
Melquisedeque Pereira <melqui1@live.com.pt>
2022-09-21 21:54:55 +0100

    Cyclic deploy attempt

--- data/seeds/04-users.js
+++ data/seeds/04-users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const config = require('../../config');
 
 console.log(config)
+console.log(process.env)
 exports.seed = (knex) => knex('users').truncate()
   .then(() => knex('users').insert([
     {
